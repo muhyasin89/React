2f84ec6a2a1de08d342174e5cdf74243a6f811e2
muhyasin89 <muhyasin89@gmail.com>
2019-08-01 16:09:18 +0700

    [UPDATE]change tutorial

--- hooks/react-academind/src/App.js
+++ hooks/react-academind/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CharPicker from './components/CharPicker';
+import Character from './components/Character';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //  const [oldState, setState] = useState({
+  //   selectedCharacter: 1,
+  //   destroyed: false
+  // })
+
+  const [selectedCharacter, setSelectedCharacter] = useState('1')
+
+  const [chosenSide, setChosenSide] = useState('light')
+
+  const [destroyed, setDestroyed] = useState(false)
+
+  const sideHandler = side => {
+    // setState({...oldState, side: side})
+    setChosenSide(side) //side is string look at button bind
+  }
+
+  const charSelectHandler = event => {
+    const charId = event.target.value;
+    // setState({...oldState, selectedCharacter: charId })
+    setSelectedCharacter(charId)
+  }
+
+  const destructionHandler = () => {
+    // setState({...oldState, desroyed: true})
+    setDestroyed(true)
+  }
+
+
+  let content = (
+      <CharPicker 
+        side={ chosenSide }
+        selectedChar={selectedCharacter}
+        onCharSelect={charSelectHandler}
+      />
+      <Character selectedChar={selectedCharacter} />
+      <button onClick={sideHandler.bind(null, 'light')}>Light Side</button>
+      <button onClick={sideHandler.bind(null, 'dark')}>Dark Side</button>
+      {oldState.side == 'dark' && (
+        <button onClick={destructionHandler}>Destroy !!</button>
+      )}
+    </React.Fragment>
+  )
+
+  return content
 }
 
 export default App;
