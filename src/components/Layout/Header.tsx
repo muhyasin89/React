74420d5cb9040ea3ad28fd5f44417db4ca2a2d15
muhyasin89 <muhyasin89@gmail.com>
2023-12-09 20:44:21 +0700

    [dashboard]add sidebar

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,67 +1,20 @@
 import React, { useState } from "react";
 
-import * as AiIcons from 'react-icons/ai';
+
 import Grid from "@mui/material/Grid";
-import styled from "styled-components";
-import { SidebarData } from "../SubData";
-import SubMenu from "../subMenu";
+
+import Sidebar from "./Sidebar";
 
 class Header extends React.Component {
   render() {
-    const [sidebar, setSidebar] = useState(false);
-
-    const showSidebar = () => setSidebar(!sidebar);
-
-    const NavIcon = styled(Link)`
-      margin-left: 2rem;
-      font-size: 2rem;
-      height: 80px;
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-    `;
-
-    const SidebarNav = styled.nav`
-      background: #15171c;
-      width: 250px;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      position: fixed;
-      top: 0;
-      left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
-      transition: 350ms;
-      z-index: 10;
-    `;
+   
 
     return (
       <Grid item xs={12}>
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a
-              href="https://flowbite.com/"
-              className="flex items-center space-x-3 rtl:space-x-reverse"
-            >
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8"
-                alt="Flowbite Logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
-              </span>
-            </a>
-
-            <SidebarNav sidebar={sidebar}>
-           
-                <NavIcon to="#">
-                  <AiIcons.AiOutlineClose onClick={showSidebar} />
-                </NavIcon>
-                {SidebarData.map((item, index) => {
-                  return <SubMenu item={item} key={index} />;
-                })}
-            
-            </SidebarNav>
+            <Sidebar />
+          
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
               <button
                 type="button"
