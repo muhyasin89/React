24b9b2be76a3b526fcd42d0e37239fde24f2f027
muhyasin89 <muhyasin89@gmail.com>
2023-12-10 12:32:30 +0700

    [auth/login-form]add login form

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Dashboard } from '@mui/icons-material';
+import Login from './page/Login';
 
 
 
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <Dashboard />
+      <Login />
     </>
   )
 }
